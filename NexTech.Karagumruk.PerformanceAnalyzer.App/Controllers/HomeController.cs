cd9edae7a20df5e1fb31b8366647840356622d4d
karacabeym <Mk142536*>
2022-03-02 01:49:33 +0300

    [Initial Commit]

---
+++ NexTech.Karagumruk.PerformanceAnalyzer.App/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NexTech.Karagumruk.PerformanceAnalyzer.App.Models;
+
+namespace NexTech.Karagumruk.PerformanceAnalyzer.App.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index(string firstName, string lastName)
+    {
+        
+        ViewBag.Name = string.Format("Name: {0} {1}", firstName, lastName);
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
