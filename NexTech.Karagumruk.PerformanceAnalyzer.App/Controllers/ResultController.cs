5c295cb814acca2284e84efb85c804a0f00171ec
Mustafa KARACABEY <89645555+Mustafa-Karacabey@users.noreply.github.com>
2022-03-02 03:27:55 +0300

    [ResultControllerImplemented ]

--- NexTech.Karagumruk.PerformanceAnalyzer.App/Controllers/ResultController.cs
+++ NexTech.Karagumruk.PerformanceAnalyzer.App/Controllers/ResultController.cs
@@ -4,11 +4,11 @@ using NexTech.Karagumruk.PerformanceAnalyzer.App.Models;
 
 namespace NexTech.Karagumruk.PerformanceAnalyzer.App.Controllers;
 
-public class HomeController : Controller
+public class ResultController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<ResultController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public ResultController(ILogger<ResultController> logger)
     {
         _logger = logger;
     }
@@ -19,16 +19,7 @@ public class HomeController : Controller
         return View();
     }
     
-    [HttpPost]
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
